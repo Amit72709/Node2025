7f393d86499bc65cdf14ac4f09bbe7cd26fd3e8a
Amit72709 <amitkumarsingh72709@gmail.com>
2025-08-02 14:48:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+fs.writeFileSync("amit.txt","my name is amit");
+
+
+// console.log(20+24)
+
+// function fruit(item){
+//     console.log(item);
+// }
+
+// fruit('apple');
+
+// var a=10;
+// var b=39;
+
+// c=a+b;
+// console.log(c);
