bccdcf1298029e17aaafa88796aea4dcc91d305b
Amit72709 <amitkumarsingh72709@gmail.com>
2025-08-02 16:15:23 +0530

    request for diffrent page

---
+++ request.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+http.createServer((req,resp)=>{
+     console.log(req.method);
+     if(req.url == "/"){
+    resp.write("<h1>Hello Amit singh home</h1>");
+     }
+     else if(req.url == "/login"){
+            resp.write("<h1>Login page</h1>");
+     }
+     else {
+        resp.write("<h1>other page</h1>");
+     }
+    resp.end();
+}).listen(5600)
