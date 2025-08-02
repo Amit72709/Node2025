715ae7736a71a25c0ee71e891072f727c0d45b06
Amit72709 <amitkumarsingh72709@gmail.com>
2025-08-02 15:18:55 +0530

    gitignore

---
+++ colors.js
@@ -0,0 +1,5 @@
+const color = require ('colors');
+
+console.log(color.red("hi g hello"));
+
+console.log(color.green("hi g hello"));
--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const http = require('http');
 
 http.createServer((req,resp)=>{
     resp.write("<h1>Hello Amit singh</h1>");
+    resp.write("like a man and ");
     resp.end('Hello');
 }).listen(4800);
